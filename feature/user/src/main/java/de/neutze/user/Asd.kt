9edd180dd1fba4bc387020e5138ec78603ebc4ac
Johannes Neutze <johannes@neutze.de>
2019-08-27 16:56:27 +0200

    Add user module (#25)

---
+++ feature/user/src/main/java/de/neutze/user/Asd.kt
@@ -0,0 +1,16 @@
+package de.neutze.user
+
+import android.content.Context
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class Asd(context: Context) {
+    private val mFirebaseAnalytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+
+    fun asd(i: Int, i1: Int): Int {
+        return i + i1
+    }
+
+    fun bsd() {
+        mFirebaseAnalytics.logEvent("test", null)
+    }
+}

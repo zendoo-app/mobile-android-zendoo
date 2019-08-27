e99cb0ae1dfaedb9af808541be23b9116a78f0bf
Johannes Neutze <johannes@neutze.de>
2019-08-27 15:34:44 +0200

    Update firebase (#14)

--- app/src/main/java/app/zendoo/zendoo/MainActivity.kt
+++ app/src/main/java/app/zendoo/zendoo/MainActivity.kt
@@ -10,7 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        val asd = Asd().asd(1, 2)
+        val asd = Asd(this)
+        asd.asd(1, 2)
+        asd.bsd()
     }
 }

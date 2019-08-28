4877db53adc3f640113f5a877c991ec6581a17cd
Johannes Neutze <johannes@neutze.de>
2019-08-28 09:10:37 +0200

    Update ColorsInstrumentedTest.kt

--- libraries/ui-components/src/androidTest/java/app/zendoo/libraries/ui/ColorsInstrumentedTest.kt
+++ libraries/ui-components/src/androidTest/java/app/zendoo/libraries/ui/ColorsInstrumentedTest.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-class ColorsTest {
+class ColorsInstrumentedTest {
 
     private lateinit var res: Resources
 

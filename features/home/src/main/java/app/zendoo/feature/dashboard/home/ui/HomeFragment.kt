0a7a92b4431f379abfc3f7528cdf5b7c8b523864
Johannes Neutze <johannes@neutze.de>
2019-09-04 20:01:43 +0200

    Add application icon

--- features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/HomeFragment.kt
+++ features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/HomeFragment.kt
@@ -1,7 +1,6 @@
 package app.zendoo.feature.dashboard.home.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup

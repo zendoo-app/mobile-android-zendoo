5032e463a7218d4bb756c1fcbb80a3eafae45a00
Johannes Neutze <johannes@neutze.de>
2019-09-04 12:48:39 +0200

    pre data binding

--- features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/entity/HomeViewEntity.kt
+++ features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/entity/HomeViewEntity.kt
@@ -1,18 +1,16 @@
 package app.zendoo.feature.dashboard.home.ui.entity
 
 import android.view.View
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import javax.inject.Inject
 
 class HomeViewEntity
 @Inject
 constructor(
-    @DrawableRes val foreground: Int?,
-    @StringRes val titleRes: Int?,
-    @StringRes val subtitleRes: Int?,
+    @StringRes val titleRes: Int,
+    @StringRes val subtitleRes: Int,
+    val progressBarVisibility: Boolean,
     val progressBarProgress: Int,
-    @DrawableRes val drawableRes: Int?,
-    @StringRes val buttonRes: Int?,
-    val buttonListener: View.OnClickListener?
+    @StringRes val buttonRes: Int,
+    val buttonListener: View.OnClickListener
 )
--- features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/entity/HomeViewEntityFactory.kt
+++ features/home/src/main/java/app/zendoo/feature/dashboard/home/ui/entity/HomeViewEntityFactory.kt
@@ -1,5 +1,6 @@
 package app.zendoo.feature.dashboard.home.ui.entity
 
+import android.view.View
 import app.zendoo.feature.dashboard.home.R
 import javax.inject.Inject
 import javax.inject.Singleton
